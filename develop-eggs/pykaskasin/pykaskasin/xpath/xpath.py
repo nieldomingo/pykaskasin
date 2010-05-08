9fec7380cdac6b74e047624caacd0782a50c66c3
Nathaniel C. Domingo <niel@NCD-Mac-Book.local>
2010-05-08 22:25:25 +0800

    finished the first version of pykaskasin.xpath

--- develop-eggs/pykaskasin/pykaskasin/xpath/xpath.py
+++ develop-eggs/pykaskasin/pykaskasin/xpath/xpath.py
@@ -1,13 +1,70 @@
 from lxml import etree
 import StringIO
 
-class KKNode:
+class KKNodeList(list):
+	"""
+	List of KKNode objects
+	"""
+	
+	def __init__(self, l=[]):
+		list.__init__(self, [KKNode(o) for o in l])
+		
+	def first(self):
+		"""
+		returns the first KKNode object
+		"""
+		
+		if len(self) < 1:
+			return None
+		else:
+			return self[0]
+			
+	def xpath(self, stmt):
+		"""
+		runs the XPATH statement on the first KKNode object
+		"""
+		
+		if len(self) < 1:
+			return KKNodeList()
+		else:
+			return KKNodeList(self[0].xpath(stmt))
+			
+	def value(self):
+		"""
+		"""
+		
+		if len(self) > 0:
+			return self[0].value()
+		else:
+			return ''
+
+class KKNode(object):
 	"""
 	DOM node wrapper
 	"""
 
 	def __init__(self, o):
 		self.o = o
+		
+	def xpath(self, stmt):
+		"""
+		runs an XPATH statement on this KKNode
+		"""
+		
+		results = self.o.xpath(stmt, smart_strings=False)
+		return KKNodeList(results)
+		
+	def value(self):
+		"""
+		returns the value of the KKNode object. returns None if the method
+		is run on a DOM tag.
+		"""
+		
+		if isinstance(self.o, str):
+			return self.o.strip()
+		else:
+			return ''
+		
 
 def ParseXML(xml):
 	"""
@@ -15,7 +72,19 @@ def ParseXML(xml):
 	the root node of the resulting DOM tree is returned as
 	a KKNode object
 	"""
+	
 	parser = etree.XMLParser()
-	o = etree.parser(StringIO.StringIO(xml), parser)
+	o = etree.parse(StringIO.StringIO(xml), parser)
 	
 	return KKNode(o)
+	
+def ParseHTML(xml):
+	"""
+	specialize parsing function for HTML
+	"""
+	
+	parser = etree.HTMLParser()
+	o = etree.parse(StringIO.StringIO(xml), parser)
+	
+	return KKNode(o)
+	
